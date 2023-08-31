62919ef52197c5b042753455ad5018470c8cfd85
Edisson David Prieto Vargas <v-edivargas@expediagroup.com>
2023-08-30 21:44:53 -0500

    UC#7 Start

---
+++ src/main/java/com/example/usecase7/UseCase7Application.java
@@ -0,0 +1,13 @@
+package com.example.usecase7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UseCase7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UseCase7Application.class, args);
+    }
+
+}
